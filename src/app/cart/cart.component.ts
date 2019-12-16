062213834b8ecf2022821f2164a47e95c8358883
lexaabrahamsen <lexaraea@gmail.com>
2019-12-16 17:27:49 -0500

    Price displaying

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { CartService } from '../cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
   items;
 
   constructor(
